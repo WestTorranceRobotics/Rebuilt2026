51fde4991b713a3d3ebd2688f78ee589c5dc8d03
apoa30 <borgybp+3@gmail.com>
2026-03-25 15:47:34 -0700

    Implement feeder sim - Shooter sim extends targetRPM - Shooter now reports feeder RPM

--- src/main/java/frc/robot/subsystems/Shooter/ShooterIOSim.java
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterIOSim.java
@@ -1,10 +1,6 @@
 package frc.robot.subsystems.Shooter;
 
-import static frc.robot.constants.ShooterConstants.*;
-
 import com.revrobotics.sim.SparkMaxSim;
-import edu.wpi.first.math.controller.BangBangController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.wpilibj.simulation.BatterySim;
@@ -14,22 +10,20 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ShooterIOSim extends ShooterIOReal implements ShooterIO {
     // TODO: implement feeder sim in periodic
-    private SparkMaxSim feederMotorSim;
+    private final SparkMaxSim feederMotorSim;
 
     private final SparkMaxSim launcherMotorLeaderSim;
     private final SparkMaxSim launcherMotorFollowerSim;
 
     private final FlywheelSim flywheelSim = new FlywheelSim(
-            LinearSystemId.createFlywheelSystem(DCMotor.getNEO(3), 0.00062156662, 1),
-            DCMotor.getNEO(3)); // TODO update physical constants
+            LinearSystemId.createFlywheelSystem(DCMotor.getNEO(3), 0.00062156662, 1), DCMotor.getNEO(3));
 
-    private final BangBangController bangbang = new BangBangController();
-    private final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(0, 0.00235); // TODO: tune further
-
-    private double targetRPM = 0;
-    private double actualRPM = 0;
+    private final FlywheelSim feederSim = new FlywheelSim(
+            LinearSystemId.createFlywheelSystem(DCMotor.getNEO(1), 0.00062156662, 1), DCMotor.getNEO(1));
 
     public ShooterIOSim() {
+        feederMotorSim = new SparkMaxSim(feederMotor, DCMotor.getNEO(1));
+
         launcherMotorLeaderSim = new SparkMaxSim(flywheelMotor, DCMotor.getNEO(1));
         launcherMotorFollowerSim = new SparkMaxSim(flywheelMotorInverted, DCMotor.getNEO(1));
     }
@@ -39,13 +33,18 @@ public class ShooterIOSim extends ShooterIOReal implements ShooterIO {
         flywheelSim.setInput(launcherMotorLeaderSim.getAppliedOutput() * RoboRioSim.getVInVoltage());
         flywheelSim.update(0.02);
 
+        feederSim.setInput(feederMotorSim.getAppliedOutput() * RoboRioSim.getVInVoltage());
+        feederSim.update(0.02);
+
         // Update motors
+        feederMotorSim.iterate(feederSim.getAngularVelocityRPM(), RoboRioSim.getVInVoltage(), 0.02);
         launcherMotorLeaderSim.iterate(flywheelSim.getAngularVelocityRPM(), RoboRioSim.getVInVoltage(), 0.02);
         launcherMotorFollowerSim.iterate(flywheelSim.getAngularVelocityRPM(), RoboRioSim.getVInVoltage(), 0.02);
 
         this.actualRPM = flywheelSim.getAngularVelocityRPM();
         SmartDashboard.putNumber("Shooter Target RPM", targetRPM);
-        SmartDashboard.putNumber("Shooter RPM", actualRPM);
+        SmartDashboard.putNumber("Shooter RPM", flywheelSim.getAngularVelocityRPM());
+        SmartDashboard.putNumber("Feeder RPM", feederSim.getAngularVelocityRPM());
 
         // TODO does this carry between sims? seems like it does
         RoboRioSim.setVInVoltage(BatterySim.calculateDefaultBatteryLoadedVoltage(flywheelSim.getCurrentDrawAmps()));
